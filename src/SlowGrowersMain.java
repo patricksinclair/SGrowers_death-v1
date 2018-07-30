0fb4a6caa126ca31ad3e4428a9f64a9ee916ee3f
Patrick Sinclair <patricksinclair@hotmail.co.uk>
2018-07-30 15:23:46 +0100

    distributions commit

--- src/SlowGrowersMain.java
+++ src/SlowGrowersMain.java
@@ -1,8 +1,9 @@
 
 public class SlowGrowersMain {
     public static void main(String[] args) {
-        SlowGrowerFrame sgFrame = new SlowGrowerFrame(); sgFrame.setVisible(true);
-        //double specific_alpha = Math.log(11.5)/500.;
+        //SlowGrowerFrame sgFrame = new SlowGrowerFrame(); sgFrame.setVisible(true);
+        double specific_alpha = Math.log(11.5)/500.;
         //BioSystem.exponentialGradient_spatialAndGRateDistributions(specific_alpha);
+        BioSystem.exponentialGradient_AllTheDistributions(specific_alpha);
     }
 }
